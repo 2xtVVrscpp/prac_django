a3a0f7ee7de72ea416a9b41797b885eb836739e6
masayuki <q232042m@mail.kyutech.jp>
2021-05-30 18:16:14 +0900

    Added view and template for detailed blog post as well as CSS for the site.

--- document/views.py
+++ document/views.py
@@ -1,4 +1,4 @@
 from django.shortcuts import render
 
 def document(request):
-  return render(request, 'document/html/index.html', {})
+  return render(request, 'document/html/knowledge/index.html', {})
